49b5cf379f581a95adeb790cb83d0f4921a3c479
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2023-01-15 15:50:16 +0600

    chatGpt-3 Text-Davinchi-003

--- server/server.js
+++ server/server.js
@@ -1,6 +1,44 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import colors from "colors";
 import { Configuration, OpenAIApi } from "openai";
 
+// inishilized the app---
+const app = express();
+// initialized the env file ---
 dotenv.config();
+
+// middleware---
+app.use(cors());
+app.use(express.json());
+
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+app.get("/", async (req, res) => {
+    res.status(200).send({
+        success: true,
+        message: "Hello World",
+    })
+})
+
+
+app.post("/", async (req, res) => {
+
+    try {
+        const prompt = req.body.prompt;
+
+
+
+    } catch (error) {
+        console.log(`error from try > catch function: ${error}`.bgRed)
+    }
+})
+
+
+
+app.listen()
