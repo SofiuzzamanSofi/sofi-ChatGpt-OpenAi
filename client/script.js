d922cb20748d28229b3caf826e9fdadd953f8ff0
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2023-03-21 02:59:53 +0600

    RENDER server is runnig at last well

--- client/script.js
+++ client/script.js
@@ -116,7 +116,7 @@ const handleSubmit = async (e) => {
   // console.log({ name: data.get('prompt') })
 
   //fetch data from server ---
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://sofi-chatgpt-ai.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
